e7400cb2c96ea587963dffbb68edf7a67db29ad2
rodolpholl <rodolpho.loreto@hotmail.com>
2018-06-16 16:08:29 -0300

    Acerto interceptor

--- ClientApp/src/app/core/Interceptor.ts
+++ ClientApp/src/app/core/Interceptor.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpInterceptor, HttpRequest, HttpHandler, HttpSentEvent, HttpHeaderResponse, HttpProgressEvent,
-  HttpResponse, HttpUserEvent, HttpErrorResponse} from '@angular/common/http';
+  HttpResponse, HttpUserEvent, HttpErrorResponse, HttpEvent} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 import {TokenStorage} from './token.storage';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
@@ -14,14 +17,15 @@ export class Interceptor implements HttpInterceptor {
   constructor(private token: TokenStorage, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler):
-    Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>> {
+    Observable<HttpEvent<any>> {
     let authReq = req;
     if (this.token.getToken() != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + this.token.getToken())});
     }
-    return next.handle(authReq).do(
+    return next.handle(authReq).catch(
         (err: any) => {
           if (err instanceof HttpErrorResponse) {
+            this.token.signOut();
             console.log(err);
             console.log('req url :: ' + req.url);
             if (err.status === 401 ||
@@ -29,6 +33,7 @@ export class Interceptor implements HttpInterceptor {
               err.status === 500) {
               this.router.navigate(['']);
             }
+            return Observable.throw(err);
           }
         }
       );
